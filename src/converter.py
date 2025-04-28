f3fc753bcb54951cbc8a77edc096d934d1eb232e
Peyman <peyman.irajizad@intel.com>
2025-04-28 15:02:47 -0700

    Add kelvin to fahrenheit converter and tests

--- src/converter.py
+++ src/converter.py
@@ -3,3 +3,9 @@ def celsius_to_kelvin(celsius: float) -> float:
     if celsius < -273.15:
         raise ValueError("Temperature below absolute zero!")
     return celsius + 273.15
+
+
+
+def kelvin_to_fahrenheit(kelvin: float) -> float:
+    """Convert Kelvin to Fahrenheit."""
+    return (kelvin - 273.15) * 9/5 + 32
--- tests/test_converter.py
+++ tests/test_converter.py
@@ -1,15 +1,15 @@
 import pytest
-from src.converter import celsius_to_kelvin
+from src.converter import celsius_to_kelvin, kelvin_to_fahrenheit
 
-def test_zero():
+def test_celsius_to_kelvin():
     assert celsius_to_kelvin(0) == 273.15
-
-def test_positive():
-    assert celsius_to_kelvin(25) == 298.15
-
-def test_negative():
-    assert celsius_to_kelvin(-273.15) == 0.0
-
-def test_below_absolute_zero():
+    assert celsius_to_kelvin(100) == 373.15
+    assert round(celsius_to_kelvin(-273.15), 2) == 0.00
     with pytest.raises(ValueError):
         celsius_to_kelvin(-274)
+
+def test_kelvin_to_fahrenheit():
+    assert round(kelvin_to_fahrenheit(273.15), 2) == 32.00
+    assert round(kelvin_to_fahrenheit(373.15), 2) == 212.00
+    assert round(kelvin_to_fahrenheit(0), 2) == -459.67
+
